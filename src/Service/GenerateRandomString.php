c89f2a3d01b730df1a1d466690990b09ef917276
joanlopes <jjlopes@huia.haus>
2022-10-03 02:29:41 -0300

    remove annotation

--- src/Service/GenerateRandomString.php
+++ src/Service/GenerateRandomString.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use Exception;
-
 class GenerateRandomString
 {
     public const RANDOM_BYTES_LENGTH = 4;
 
-    /**
-     * @throws Exception
-     */
     public function getRandomString(): string
     {
         $bytes = random_bytes(self::RANDOM_BYTES_LENGTH);
